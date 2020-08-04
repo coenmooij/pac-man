0ead8a8964b80168eb72db443518898dea70f683
Coen Mooij <coenmooij@gmail.com>
2020-08-04 21:12:43 +0200

    refactor constants, add to grid + walls, make movement smooth

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
+import { CANVAS } from './lib/canvas.js';
 import Controller from './lib/controller.js';
 import Grid from './lib/grid.js';
 import Maze from './lib/maze.js';
 import PacMan from './lib/pac-man.js';
 
 const SPEED = 150;
-const SIZE = 600;
 
 let context;
 let pacMan;
@@ -22,15 +22,15 @@ window.onload = function () {
 
     // Listen to inputs and start the game
     document.addEventListener('keydown', (event) => {
-        controller.onKeydown(event);
+        controller.onKeyDown(event);
     });
     setInterval(update, 1000 / SPEED);
 }
 
 function initialize() {
     maze = new Maze(context);
-    grid = new Grid();
-    pacMan = new PacMan(context, SIZE / 2, SIZE / 2);
+    grid = new Grid(context);
+    pacMan = new PacMan(context, CANVAS.GAP, CANVAS.GAP); // TODO : Set starting point properly
     controller = new Controller(grid, pacMan);
 }
 
@@ -38,5 +38,6 @@ function update() {
     controller.move();
     maze.reset();
     maze.paint();
+    grid.paint(); // TODO : only enable for debug
     pacMan.paint();
 }
