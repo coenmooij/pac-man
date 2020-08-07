41fbcffee2f73bdd40353a44e948169fb4008228
Coen Mooij <coenmooij@gmail.com>
2020-08-07 12:47:50 +0200

    [wip] rework

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
-import { CANVAS } from './lib/constants/canvas.js';
 import Controller from './lib/controller.js';
-import Grid from './lib/grid.js';
+import MazePainter from './lib/maze-painter.js';
+import Maze from './lib/maze.js';
+import PacManPainter from './lib/pac-man-painter.js';
 import PacMan from './lib/pac-man.js';
+
+const FPS = 24;
 
 let context;
 let pacMan;
-let grid;
 let maze;
 let controller;
+let mazePainter;
+let pacManPainter;
 
 window.onload = function () {
     // Get the context
@@ -21,18 +25,28 @@ window.onload = function () {
     document.addEventListener('keydown', (event) => {
         controller.onKeyDown(event);
     });
-    setInterval(update, 5);
+    setInterval(update, 1000 / FPS);
 }
 
 function initialize() {
-    grid = new Grid(context);
-    pacMan = new PacMan(context);
-    controller = new Controller(grid, pacMan);
+    maze = new Maze();
+    mazePainter = new MazePainter(context, maze);
+    mazePainter.paint();
+
+    pacMan = new PacMan();
+    pacManPainter = new PacManPainter(context, pacMan);
+    pacManPainter.paint();
+
+    controller = new Controller(maze, pacMan);
+
 }
 
 function update() {
-    pacMan.reset();
     controller.move();
     pacMan.bite();
-    pacMan.paint();
+
+    // For performance we only repaint the part of the board that will change
+    // TODO : Add ghost locations to repaint
+    mazePainter.repaint();
+    pacManPainter.paint();
 }
